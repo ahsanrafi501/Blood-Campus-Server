34c4d22ae5607d96a0d860b9a237b85ad1b195bb
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-02-25 16:19:43 +0600

    express listener and app structure done

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+
+
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.cors_origin,
+    credentials: true,
+}))
+
+
+app.use(express.json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kbg' }))
+app.use(express.static("Public"))
+app.use(cookieParser())
+
+
+export {app}
