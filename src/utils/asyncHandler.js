9cbd1cdfb106c5ceb660256155a549797607f04e
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-02-25 21:29:59 +0600

    api error custom code done

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
-const asyncHandler = (requestHandler) =>{
-    (req, res, next) =>{
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch(error => next(error))
     }
 }
 
-export {asyncHandler}
+export { asyncHandler }
